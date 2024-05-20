d165f2954d238557e93e3fc64ac35e033e94dc45
Francesco142 <francesco.impellizzeri02@gmail.com>
2024-05-20 18:12:11 +0200

    Docker microservizi

---
+++ src/test/java/com/ShopAndCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAndCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
